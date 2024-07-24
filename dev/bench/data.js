ebc17af6bcd8b3f56a6fc53250111958542502c0
github-action-benchmark <github@users.noreply.github.com>
2024-07-24 18:46:57 +0000

    add Benchmark (benchmarkjs) benchmark result for ecf074c8c6d5ed67eed2e490c653b41aae691c19

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1721846816700,
+  "repoUrl": "https://github.com/Yogesh01000100/cacti",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "michal.bajer@fujitsu.com",
+            "name": "Michal Bajer",
+            "username": "outSH"
+          },
+          "committer": {
+            "email": "9387513+outSH@users.noreply.github.com",
+            "name": "Michal Bajer",
+            "username": "outSH"
+          },
+          "distinct": true,
+          "id": "ecf074c8c6d5ed67eed2e490c653b41aae691c19",
+          "message": "feat(ledger-browser): rewrite fabric application\n\n- Rewrite fabric app using MUI components and new database schema.\n- Improve `UITableListing` to support clickable tables.\n- The new app supports the following views:\n  - Dashboard: Shows summary of blocks and transaction recorded in database.\n  - Block list: Full list of blocks\n  - Transaction list: Full list of transactions\n  - Transaction details: Page that shows full transaction information,\n    transaction actions (method calls) and endorsements.\n\nDepends on #3308\nDepends on #3279\n\nSigned-off-by: Michal Bajer <michal.bajer@fujitsu.com>",
+          "timestamp": "2024-07-24T18:36:16+02:00",
+          "tree_id": "ba27242dff32b811849a51578b97d73ce94d0b08",
+          "url": "https://github.com/Yogesh01000100/cacti/commit/ecf074c8c6d5ed67eed2e490c653b41aae691c19"
+        },
+        "date": 1721846814645,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 595,
+            "range": "±1.63%",
+            "unit": "ops/sec",
+            "extra": "178 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 367,
+            "range": "±1.32%",
+            "unit": "ops/sec",
+            "extra": "182 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
