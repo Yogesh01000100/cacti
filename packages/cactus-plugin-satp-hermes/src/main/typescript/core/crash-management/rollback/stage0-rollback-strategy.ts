14059231bd02783e5f0349d616c7ddde43a42f02
Yogesh01000100 <yogeshone678@gmail.com>
2024-08-20 21:08:54 +0000

    feat(satp-hermes): add crash recovery & rollback protocol
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    chore(satp-hermes): improve DB management
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    chore(satp-hermes): crash recovery architecture
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    fix(recovery): enhance crash recovery and rollback implementation
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    refactor(recovery): consolidate logic and improve SATP message handling
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    feat(recovery): add rollback implementations
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: correct return types and inits
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    Co-authored-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    fix: add unit tests and resolve rollbackstate
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    feat: add function processing logs from g2
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    feat: add cron schedule for periodic crash checks
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: resolve rollback condition and add tests
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    feat: add orchestrator communication layer using connect-RPC
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    feat: add rollback protocol rpc
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: handle server log synchronization
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: resolve gol errors, add unit tests
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: handle server-side rollback
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: resolve networkId in rollback strategies
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>

---
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/crash-management/rollback/stage0-rollback-strategy.ts
@@ -0,0 +1,156 @@
+import { Logger, LoggerProvider } from "@hyperledger/cactus-common";
+import { SATPSession } from "../../satp-session";
+import { RollbackStrategy } from "./rollback-strategy-factory";
+import {
+  RollbackLogEntrySchema,
+  RollbackState,
+  RollbackStateSchema,
+} from "../../../generated/proto/cacti/satp/v02/crash_recovery_pb";
+import { ILocalLogRepository } from "../../../repository/interfaces/repository";
+import { create } from "@bufbuild/protobuf";
+import { SATPBridgesManager } from "../../../gol/satp-bridges-manager";
+
+export class Stage0RollbackStrategy implements RollbackStrategy {
+  private log: Logger;
+  private logRepository: ILocalLogRepository;
+  private bridgeManager: SATPBridgesManager;
+
+  constructor(
+    bridgeManager: SATPBridgesManager,
+    localLog: ILocalLogRepository,
+  ) {
+    this.log = LoggerProvider.getOrCreate({ label: "Stage0RollbackStrategy" });
+    this.logRepository = localLog;
+    this.bridgeManager = bridgeManager;
+  }
+
+  async execute(session: SATPSession): Promise<RollbackState> {
+    const fnTag = "Stage0RollbackStrategy#execute";
+    this.log.info(`${fnTag} Executing rollback for Stage 0`);
+
+    if (!session) {
+      throw new Error(`${fnTag}, session data is not correctly initialized`);
+    }
+    const sessionData = session.hasClientSessionData()
+      ? session.getClientSessionData()
+      : session.getServerSessionData();
+
+    if (!sessionData) {
+      throw new Error(`${fnTag}, session data is not correctly initialized`);
+    }
+
+    const isClient = session.hasClientSessionData();
+    const network = isClient
+      ? sessionData.senderGatewayNetworkId
+      : sessionData.recipientGatewayNetworkId;
+
+    if (isClient && !network) {
+      throw new Error(
+        `${fnTag}: Unable to determine network from session data.`,
+      );
+    }
+
+    this.log.info(`${fnTag} network: ${network}`);
+
+    const bridge = this.bridgeManager.getBridge(network);
+    if (!bridge) {
+      throw new Error(`${fnTag}: No bridge found for network: ${network}`);
+    }
+
+    const rollbackState = create(RollbackStateSchema, {
+      sessionId: session.getSessionId(),
+      currentStage: "Stage0",
+      stepsRemaining: isClient ? 1 : 0,
+      rollbackLogEntries: [],
+      estimatedTimeToCompletion: "",
+      status: "IN_PROGRESS",
+      details: "",
+    });
+
+    try {
+      if (isClient) {
+        // Client-side:
+        const assetId = sessionData.senderAsset?.tokenId;
+
+        if (!assetId) {
+          throw new Error(`${fnTag}: Asset ID is undefined`);
+        }
+
+        this.log.info(`${fnTag} Unwrapping Asset ID: ${assetId}`);
+        await bridge.unwrapAsset(assetId);
+
+        const rollbackLogEntry = create(RollbackLogEntrySchema, {
+          sessionId: session.getSessionId(),
+          stage: "Stage0",
+          timestamp: new Date().toISOString(),
+          action: "UNWRAP_ASSET",
+          status: "SUCCESS",
+          details: "",
+        });
+
+        rollbackState.rollbackLogEntries.push(rollbackLogEntry);
+        rollbackState.stepsRemaining = 0;
+        rollbackState.status = "COMPLETED";
+        rollbackState.details = "Rollback of Stage 0 completed successfully";
+      } else {
+        // Server-side:
+        const rollbackLogEntry = create(RollbackLogEntrySchema, {
+          sessionId: session.getSessionId(),
+          stage: "Stage0",
+          timestamp: new Date().toISOString(),
+          action: "NO_ACTION_REQUIRED",
+          status: "SUCCESS",
+          details: "",
+        });
+
+        rollbackState.rollbackLogEntries.push(rollbackLogEntry);
+        rollbackState.status = "COMPLETED";
+        rollbackState.details = "";
+      }
+
+      this.log.info(`${fnTag} Rollback of Stage 0 completed successfully`);
+      return rollbackState;
+    } catch (error) {
+      this.log.error(`${fnTag} Error during rollback of Stage 0: ${error}`);
+
+      const rollbackLogEntry = create(RollbackLogEntrySchema, {
+        sessionId: session.getSessionId(),
+        stage: "Stage0",
+        timestamp: new Date().toISOString(),
+        action: isClient ? "UNWRAP_ASSET" : "NO_ACTION_REQUIRED",
+        status: "FAILED",
+        details: `Rollback of Stage 0 failed: ${error}`,
+      });
+
+      rollbackState.rollbackLogEntries.push(rollbackLogEntry);
+      rollbackState.status = "FAILED";
+      rollbackState.details = `Rollback of Stage 0 failed: ${error}`;
+
+      return rollbackState;
+    }
+  }
+
+  async cleanup(
+    session: SATPSession,
+    state: RollbackState,
+  ): Promise<RollbackState> {
+    const fnTag = "Stage0RollbackStrategy#cleanup";
+    this.log.info(`${fnTag} Cleaning up after Stage 0 rollback`);
+
+    if (!session) {
+      this.log.error(`${fnTag} Session not found`);
+      return state;
+    }
+
+    try {
+      // TODO: Implement Stage 0 specific cleanup logic
+
+      // TODO: Update other state properties as needed
+
+      return state;
+    } catch (error) {
+      this.log.error(`${fnTag} Cleanup failed: ${error}`);
+      return state;
+    }
+  }
+}
